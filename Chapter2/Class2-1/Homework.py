5eae6936c2829d1c59a68433ec296693a2758b48
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2023-02-22 21:35:09 -0500

    Class 2 -1

---
+++ Chapter2/Class2-1/Homework.py
@@ -0,0 +1,15 @@
+# Funcion que devuelve una lista invertida de manera recurrente
+# Se puede utilizar el concat de python + o  [:]
+# No es permitido utilizar funciones build in <nombre>(_)
+# solo es pormitido utiliar pop()
+
+def reverse_recursive(lista,lista_inv_accum,idx):
+    pass 
+
+# Suma de una lista  ...
+def sum_recursive():
+    pass 
+
+# encontrar el maximo  ...
+def max_recursive():
+    pass 
