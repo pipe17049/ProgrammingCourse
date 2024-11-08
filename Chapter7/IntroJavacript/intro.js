af9ac266a8eba366326d00b5bad23e92e192bc40
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2024-11-08 08:07:05 -0500

    Some reference info

--- Chapter7/IntroJavacript/intro.js
+++ Chapter7/IntroJavacript/intro.js
@@ -1,10 +1,11 @@
 // Declare
 
 var foo = 4;
-var bar = "other"
+var bar = "other" // ; optional
 let number = 3; 
 const pi = 3.1416;
 // let number = 5;
+// pi = 2.4;
 
 // If sentence
 
@@ -18,35 +19,68 @@ if (foo > 4){
 
 // For sentence
 
-for(let i= 0; i<=10 ; i++){
+for(let i= 0; i<=10 ; i= i+1){
     console.log("For :", i)
 }
-
 // Let scope , is not possible
 // console.log(i)
-let j = 0
-while(j<10){
-    console.log("While j", j)
-    j++
+
+let j = "";
+while(j!="AAA"){
+    console.log("While j", j);
+    j = j+"A"
 }
 
 // Function
 function myFunction(name){
     // Reverse ` and place holder ${} 
-    console.log(`Hello ${name}, How are you ${name}?`)
+    console.log(`Hello ${name}, How are you ${name}?`);
 }
 myFunction("Eduardo")
 
 // Lists
-const list = []
+const list = [];
 list.push("Red")
 list.push("White")
 console.log(`First element list : ${list[0]}`) // Get an element
 // list = ["A"] not allow change the reference
 
-// other for using list as a direct element
-for (element in list){
-    console.log(`Element of list: ${element}`)
+// By value
+let n = 0
+function add(n,times){
+    for (let i= 1; i<= times; i++){
+        n = n +1;
+    }    
+    console.log(`N inside add fun ${n}`);
+    return n;
+}
+add(n, 20)
+console.log(`N after call fun ${n}`)
+
+// By reference
+const listNames = ["Eduardo", "Carlos"];
+
+function addNames(list,newName){
+    listNames.push(newName);
+}
+addNames(listNames,"Camila");
+console.log(`listNames after call fun ${listNames}`);
+
+
+// other for using list as index in
+for (index in listNames){
+    console.log(`Index of list: ${index}, 
+        element of the list ${listNames[index]}`)
+}
+
+// direct element
+for (const element of listNames){
+    console.log(`Element of list ${element}`)
+}
+
+// print list with limits
+for (let i = 0; i<listNames.length ; i++){
+    console.log(`Elemento ${i}, ${listNames[i]}`)
 }
 
 // JSON 
@@ -62,6 +96,8 @@ const dev = {
         languages: ["Python", "Scala", "Clojure", "Java"] 
     }
 }
+
+
 const devs = [dev,{name: "Camila", experience: ["Typescript","Angular"]}]
 // Access data, root properties
 console.log(`Name: ${dev.name}`)
