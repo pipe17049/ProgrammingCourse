b5449ce3e935c9bc538c8aa7929f3b404a6280fc
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2024-11-17 09:01:50 -0500

    Notes

--- Chapter2_Algorithms/Class2-0_Diagrams/Homework1-1.py
+++ Chapter2_Algorithms/Class2-0_Diagrams/Homework1-1.py
@@ -4,10 +4,11 @@ import Homework
 def main():
     double = [True, True, True, True, True,True,True]  
     single = [True, True, True, True, True,True]
-    no_spicy = []  
-    no_vegetables = []
+    no_spicy = []   # create case
+    no_vegetables = [] #create case 
 
     double_price = Homework.price_hamburger(*double) # Why I have to use * ?
     print(double_price)
+    # print cases
 
 main()
--- Chapter2_Algorithms/Class2-0_Diagrams/Homework2.py
+++ Chapter2_Algorithms/Class2-0_Diagrams/Homework2.py
@@ -4,6 +4,20 @@ import fileinput
 L1 and L2 always be ordered, but not sequencial , can have repeated numbers
 You must create a new array with the numbers of both lists ordered !  
 Is posible that len(L1) != len(L2) take care ! 
+C1
+[-20,2,2,4,5,8], [-1,0,1,3,6,7]
+
+[-20,-1,0,1,1,2,2,3,4,5,6,7,8]
+
+C2
+[1,2,3] , [4,5,6]
+
+[1,2,3,4,5,6]
+
+C3
+[1,2] , [4,5,6]
+
+[1,2,4,5,6]
 """
 
 def merge_list_ordered(L1, L2):
