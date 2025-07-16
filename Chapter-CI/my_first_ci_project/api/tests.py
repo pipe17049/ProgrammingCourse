d9ad9c8cb9cb5c56f84c451c3a3491db7b2cb23c
AriasAEnima <julianeduardoarias@outlook.com>
2025-07-15 20:35:54 -0500

    prueba

---
+++ Chapter-CI/my_first_ci_project/api/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class ProductDetailTest(TestCase):
+    def test_product_detail(self):
+        response = self.client.get('/api/product/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(
+            response.content,
+            {"product": "random-name", "id": 1}
+        )
