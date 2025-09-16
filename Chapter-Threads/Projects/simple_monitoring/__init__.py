cc24f00ac4f08cf4bd5c5dcfe1edfab6e319b26c
J. Eduardo Arias <julianeduardoarias@outlook.com>
2025-09-16 16:37:25 -0500

    Rename some things :P

--- Chapter-Threads/Projects/simple_monitoring/__init__.py
+++
@@ -1,6 +0,0 @@
-# Simple Monitoring package - Metrics + Recommendations (Read-Only)
-from .metrics_collector import SimpleMetricsCollector
-from .recommendations import ScalingRecommendations
-from .dashboard import SimpleDashboard
-
-__all__ = ['SimpleMetricsCollector', 'ScalingRecommendations', 'SimpleDashboard']
